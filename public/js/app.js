53551ba413efaa6437bbc62f20fe94ec9f1979a6
ezecuenca <ezekielraine12@gmail.com>
2026-01-27 15:24:15 +0800

    fix run watch not working

--- public/js/app.js
+++ public/js/app.js
@@ -64801,9 +64801,12 @@ var FormProvider = function FormProvider(_ref) {
 /*!*********************************!*\
   !*** ./resources/scss/app.scss ***!
   \*********************************/
-() {
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"{\".\n   ╷\n16 │         display: flex;\r\n   │                      ^\n   ╵\n  resources\\scss\\adr-form.scss 16:22  @import\n  resources\\scss\\app.scss 25:9        root stylesheet\n    at processResult (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\webpack\\lib\\NormalModule.js:940:19)\n    at C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\webpack\\lib\\NormalModule.js:1141:5\n    at C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\loader-runner\\lib\\LoaderRunner.js:499:11\n    at runSyncOrAsyncCallback (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\loader-runner\\lib\\LoaderRunner.js:259:19)\n    at innerCallback (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\loader-runner\\lib\\LoaderRunner.js:175:13)\n    at C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass-loader\\dist\\index.js:49:7\n    at Function.call$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:132459:16)\n    at render_closure1.call$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:112393:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:40280:18)\n    at _FutureListener.handleError$1 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38811:21)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:39112:49)\n    at Object._Future__propagateToListeners (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:5217:77)\n    at _Future._completeErrorObject$1 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38972:9)\n    at _AsyncAwaitCompleter.completeError$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38582:12)\n    at Object._asyncRethrow (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:4980:17)\n    at C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:28970:20\n    at _wrapJsFunctionForAsync_closure.$protected (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:5005:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38601:12)\n    at _awaitOnObject_closure0.call$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38595:25)\n    at _RootZone.runBinary$3$3 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:40280:18)\n    at _FutureListener.handleError$1 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38811:21)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:39112:49)\n    at Object._Future__propagateToListeners (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:5217:77)\n    at _Future._completeErrorObject$1 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38972:9)\n    at _AsyncAwaitCompleter.completeError$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38582:12)\n    at Object._asyncRethrow (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:4980:17)\n    at C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:20873:20\n    at _wrapJsFunctionForAsync_closure.$protected (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:5005:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38601:12)\n    at _awaitOnObject_closure0.call$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38595:25)\n    at _RootZone.runBinary$3$3 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:40280:18)\n    at _FutureListener.handleError$1 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38811:21)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:39112:49)\n    at Object._Future__propagateToListeners (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:5217:77)\n    at _Future._completeErrorObject$1 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38972:9)\n    at _AsyncAwaitCompleter.completeError$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38582:12)\n    at Object._asyncRethrow (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:4980:17)\n    at C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:20922:20\n    at _wrapJsFunctionForAsync_closure.$protected (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:5005:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38601:12)\n    at _awaitOnObject_closure0.call$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38595:25)\n    at _RootZone.runBinary$3$3 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:40280:18)\n    at _FutureListener.handleError$1 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38811:21)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:39112:49)\n    at Object._Future__propagateToListeners (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:5217:77)\n    at _Future._completeErrorObject$1 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38972:9)\n    at _AsyncAwaitCompleter.completeError$2 (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:38582:12)\n    at Object._asyncRethrow (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:4980:17)\n    at C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:88676:24\n    at _wrapJsFunctionForAsync_closure.$protected (C:\\Users\\User\\Projects\\OCD_form_system\\node_modules\\sass\\sass.dart.js:5005:15)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }
 
@@ -64842,7 +64845,42 @@ throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugi
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -64934,13 +64972,68 @@ throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugi
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/js/app": 0,
+/******/ 			"css/app": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	__webpack_require__("./resources/js/app.js");
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
-/******/ 	var __webpack_exports__ = __webpack_require__("./resources/scss/app.scss");
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/scss/app.scss")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
 ;
