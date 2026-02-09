8e155d5179d2b37187ec3ce88beac468edbcf7a7
ezecuenca <ezekielraine12@gmail.com>
2026-02-09 16:13:49 +0800

    save before merge

--- public/js/app.js
+++ public/js/app.js
@@ -66744,7 +66744,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/dist/index.js");
 /* harmony import */ var _TasksModal__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./TasksModal */ "./resources/js/components/TasksModal.js");
-Object(function webpackMissingModule() { var e = new Error("Cannot find module '../utils/swapRequests'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+/* harmony import */ var _utils_swapRequests__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../utils/swapRequests */ "./resources/js/utils/swapRequests.js");
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
 function _toConsumableArray(r) { return _arrayWithoutHoles(r) || _iterableToArray(r) || _unsupportedIterableToArray(r) || _nonIterableSpread(); }
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
@@ -66870,7 +66870,7 @@ function Schedule() {
       status: 'pending',
       createdAt: new Date().toISOString()
     };
-    Object(function webpackMissingModule() { var e = new Error("Cannot find module '../utils/swapRequests'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(swapRequest);
+    (0,_utils_swapRequests__WEBPACK_IMPORTED_MODULE_3__.saveSwapRequest)(swapRequest);
     setShowTaskForm(false);
     setModalMode('add');
     setSelectedTask(null);
@@ -67281,7 +67281,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-Object(function webpackMissingModule() { var e = new Error("Cannot find module '../utils/swapRequests'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+/* harmony import */ var _utils_swapRequests__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../utils/swapRequests */ "./resources/js/utils/swapRequests.js");
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
 function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
@@ -67323,9 +67323,9 @@ function SwapForm() {
     swapRequests = _useState2[0],
     setSwapRequests = _useState2[1];
   (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function () {
-    setSwapRequests(Object(function webpackMissingModule() { var e = new Error("Cannot find module '../utils/swapRequests'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())());
+    setSwapRequests((0,_utils_swapRequests__WEBPACK_IMPORTED_MODULE_1__.getSwapRequests)());
     var handleStorage = function handleStorage() {
-      return setSwapRequests(Object(function webpackMissingModule() { var e = new Error("Cannot find module '../utils/swapRequests'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())());
+      return setSwapRequests((0,_utils_swapRequests__WEBPACK_IMPORTED_MODULE_1__.getSwapRequests)());
     };
     window.addEventListener('storage', handleStorage);
     return function () {
@@ -67334,14 +67334,14 @@ function SwapForm() {
   }, []);
   var handleDeny = function handleDeny(id) {
     if (window.confirm('Deny this swap request?')) {
-      Object(function webpackMissingModule() { var e = new Error("Cannot find module '../utils/swapRequests'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(id, 'denied');
-      setSwapRequests(Object(function webpackMissingModule() { var e = new Error("Cannot find module '../utils/swapRequests'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())());
+      (0,_utils_swapRequests__WEBPACK_IMPORTED_MODULE_1__.updateSwapRequestStatus)(id, 'denied');
+      setSwapRequests((0,_utils_swapRequests__WEBPACK_IMPORTED_MODULE_1__.getSwapRequests)());
     }
   };
   var handleApprove = function handleApprove(id) {
-    var success = Object(function webpackMissingModule() { var e = new Error("Cannot find module '../utils/swapRequests'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(id);
+    var success = (0,_utils_swapRequests__WEBPACK_IMPORTED_MODULE_1__.executeSwapRequest)(id);
     if (success) {
-      setSwapRequests(Object(function webpackMissingModule() { var e = new Error("Cannot find module '../utils/swapRequests'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())());
+      setSwapRequests((0,_utils_swapRequests__WEBPACK_IMPORTED_MODULE_1__.getSwapRequests)());
     } else {
       alert('Could not execute swap. The task may have been modified or removed.');
     }
@@ -68118,6 +68118,95 @@ var FormProvider = function FormProvider(_ref) {
     children: children
   });
 };
+
+/***/ },
+
+/***/ "./resources/js/utils/swapRequests.js"
+/*!********************************************!*\
+  !*** ./resources/js/utils/swapRequests.js ***!
+  \********************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   SWAP_REQUESTS_KEY: () => (/* binding */ SWAP_REQUESTS_KEY),
+/* harmony export */   executeSwapRequest: () => (/* binding */ executeSwapRequest),
+/* harmony export */   getSwapRequests: () => (/* binding */ getSwapRequests),
+/* harmony export */   removeSwapRequest: () => (/* binding */ removeSwapRequest),
+/* harmony export */   saveSwapRequest: () => (/* binding */ saveSwapRequest),
+/* harmony export */   updateSwapRequestStatus: () => (/* binding */ updateSwapRequestStatus)
+/* harmony export */ });
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+var SWAP_REQUESTS_KEY = 'swapRequests';
+var SCHEDULED_TASKS_KEY = 'scheduledTasks';
+function getSwapRequests() {
+  try {
+    var saved = localStorage.getItem(SWAP_REQUESTS_KEY);
+    return saved ? JSON.parse(saved) : [];
+  } catch (_unused) {
+    return [];
+  }
+}
+function saveSwapRequest(request) {
+  var requests = getSwapRequests();
+  requests.unshift(request);
+  localStorage.setItem(SWAP_REQUESTS_KEY, JSON.stringify(requests));
+}
+function removeSwapRequest(id) {
+  var requests = getSwapRequests().filter(function (r) {
+    return r.id !== id;
+  });
+  localStorage.setItem(SWAP_REQUESTS_KEY, JSON.stringify(requests));
+}
+function updateSwapRequestStatus(id, status) {
+  var requests = getSwapRequests().map(function (r) {
+    return r.id === id ? _objectSpread(_objectSpread({}, r), {}, {
+      status: status
+    }) : r;
+  });
+  localStorage.setItem(SWAP_REQUESTS_KEY, JSON.stringify(requests));
+}
+function executeSwapRequest(id) {
+  var requests = getSwapRequests();
+  var req = requests.find(function (r) {
+    return r.id === id;
+  });
+  if (!req || req.status !== 'pending') return false;
+  var savedTasks = localStorage.getItem(SCHEDULED_TASKS_KEY);
+  var tasks = savedTasks ? JSON.parse(savedTasks) : [];
+  var sourceTask = tasks.find(function (t) {
+    return t.name === req.taskName && t.task === req.taskDescription && t.date === req.fromDate;
+  });
+  if (!sourceTask) return false;
+  var targetTaskOnDate = tasks.find(function (t) {
+    return t.date === req.toDate;
+  });
+  var swappedAt = new Date().toISOString();
+  var newTasks = tasks.map(function (t) {
+    if (t === sourceTask) {
+      return _objectSpread(_objectSpread({}, t), {}, {
+        date: req.toDate,
+        swappedAt: swappedAt
+      });
+    }
+    if (targetTaskOnDate && t === targetTaskOnDate) {
+      return _objectSpread(_objectSpread({}, t), {}, {
+        date: req.fromDate,
+        swappedAt: swappedAt
+      });
+    }
+    return t;
+  });
+  localStorage.setItem(SCHEDULED_TASKS_KEY, JSON.stringify(newTasks));
+  updateSwapRequestStatus(id, 'approved');
+  return true;
+}
 
 /***/ },
 
